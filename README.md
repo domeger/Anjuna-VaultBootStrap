ef2a506575110369977f1c00101fcc22224ff333
Domnick Eger <deger@redlineanalytics.net>
2022-06-06 11:10:06 -0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,135 @@
-# Anjuna-VaultBootStrap
-Bootstrap Process with Vault + Consul
+[![GitHub Release](https://img.shields.io/github/release/dani-garcia/vaultwarden.svg)](https://github.com/domeger/Anjuna-VaultWithConsul/releases/latest)
+[![GPL-3.0 Licensed](https://img.shields.io/github/license/dani-garcia/vaultwarden.svg)](https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# Anjuna Security: Hashicorp Vault with Consul
+
+**Requirements:** Amazon AMI\
+**Technologies:** Vault, Consul, Anjuna Runtime, and Docker\
+**Instance Type:** m5.xlarge or higher instance
+
+# Instance Preparation 
+Complete the steps in our docs to be able to complete this step by step setup of Vault with Consul storage.
+
+**Getting Started:**
+[Anjuna Security Nitro Onboarding Tutorial](https://docs.anjuna.io/anjuna-nitro-runtime/anjuna-nitro/latest/getting_started/getting_the_runtime.html)
+
+# Consul Setup
+**Step 1:**
+`sudo yum install -y yum-utils`
+
+**Step 2:** 
+`sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo`
+
+**Step 3**
+`sudo yum -y install consul`
+
+**Step 4**
+Run the following command in a background process
+`consul agent -server -advertise=127.0.0.1 -data-dir=consul/data/ -bootstrap &`
+
+# Docker Setup
+**Step 1:**
+Installing Docker
+`sudo yum install docker`
+
+**Step 2:**
+Enabling Docker
+`sudo systemctl enable docker`
+
+**Step 3:**
+Starting Docker
+`sudo systemctl start docker`
+
+** Step 4:**
+Verifying Docker is running.
+`sudo systemctl status docker`
+
+# Vault Client Setup
+**Step 1:**
+`sudo yum install -y yum-utils`
+
+**Step 2:**
+`sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo`
+
+**Step 3:**
+`sudo yum -y install vault`
+
+# Vault Setup (Sealed Version)
+
+**Step 1:**
+Clone this repo to your /home/ec2-user/ instance and edit the config.json file.
+`vi config.json`
+
+**Step 2:**
+In the config file change localhost to 192.168.127.254 as this is the static ip of the enclave.
+```
+
+SKIP_SETCAP = true
+disable_mlock = true
+
+storage "consul" {
+address = "192.168.127.254:8500"
+path = "vault"
+}
+
+listener "tcp" {
+address = ":8200"
+tls_disable = 1
+}
+
+```
+
+**Step 3:**
+In this step were going to build the docker image.
+`docker build -t vault .`
+
+**Step 4:**
+Expose the port so the Enclave can talk to the EC2 Node
+`/opt/anjuna/nitro/bin/anjuna-nitro-netd-parent --expose 8200 --daemonize`
+
+
+**Step 5:**
+We will than build the file and push it into the enclave environment.
+`anjuna-nitro-cli build-enclave --docker-uri vault:latest --enclave-config-file enclave-config.yaml --output-file vault.eif`
+
+Now Let Run the Enclave
+```
+anjuna-nitro-cli run-enclave \
+ --cpu-count 2 \
+ --memory 2048 \
+ --eif-path vault.eif
+ ```
+
+**Step 6:**
+We will than verify the enclave is running.
+`anjuna-nitro-cli describe-enclaves | jq`
+
+![Nitro Status](https://github.com/domeger/Anjuna-VaultWithConsul/blob/main/EnclaveStatus.png)
+
+**Step 7:**
+Verify you can communicate with your Vault instance
+```
+curl -s http://localhost:8200/v1/sys/health | jq -r 
+export VAULT_ADDR='http://localhost:8200'
+export VAULT_TOKEN=
+vault status
+```
+
+![Vault Status](https://github.com/domeger/Anjuna-VaultWithConsul/blob/main/VaultStatus.png)
+
+**Step 8:**
+Initialize the Vault Repo and make sure you copy the keys that are display on your screen, these will be necessary to unseal vault to access it.
+
+```vault operator init```
+
+
+**Optional:**
+Terminate the instance if your done testing.
+
+`anjuna-nitro-cli terminate-enclave`
+
+# Copying or Reusing
+
+This project has mixed licencing. You are free to copy, redistribute and/or modify aspects of this work under the terms of each licence accordingly (unless otherwise specified).
+
+Included scripts are free software licenced under the terms of the [GNU General Public License, version 3](https://www.gnu.org/licenses/gpl-3.0.txt).
---
+++ deployment.sh
@@ -0,0 +1,145 @@
+if [ -f .env ]; then
+  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+fi
+
+cat << "EOF"
+ ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄     ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ 
+▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌   ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+▐░█▀▀▀▀▀▀▀█░▌▐░▌░▌     ▐░▌ ▀▀▀▀▀█░█▀▀▀ ▐░▌       ▐░▌▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀█░▌    ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌
+▐░▌       ▐░▌▐░▌▐░▌    ▐░▌      ▐░▌    ▐░▌       ▐░▌▐░▌▐░▌    ▐░▌▐░▌       ▐░▌        ▐░▌     ▐░▌       ▐░▌
+▐░█▄▄▄▄▄▄▄█░▌▐░▌ ▐░▌   ▐░▌      ▐░▌    ▐░▌       ▐░▌▐░▌ ▐░▌   ▐░▌▐░█▄▄▄▄▄▄▄█░▌        ▐░▌     ▐░▌       ▐░▌
+▐░░░░░░░░░░░▌▐░▌  ▐░▌  ▐░▌      ▐░▌    ▐░▌       ▐░▌▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌        ▐░▌     ▐░▌       ▐░▌
+▐░█▀▀▀▀▀▀▀█░▌▐░▌   ▐░▌ ▐░▌      ▐░▌    ▐░▌       ▐░▌▐░▌   ▐░▌ ▐░▌▐░█▀▀▀▀▀▀▀█░▌        ▐░▌     ▐░▌       ▐░▌
+▐░▌       ▐░▌▐░▌    ▐░▌▐░▌      ▐░▌    ▐░▌       ▐░▌▐░▌    ▐░▌▐░▌▐░▌       ▐░▌        ▐░▌     ▐░▌       ▐░▌
+▐░▌       ▐░▌▐░▌     ▐░▐░▌ ▄▄▄▄▄█░▌    ▐░█▄▄▄▄▄▄▄█░▌▐░▌     ▐░▐░▌▐░▌       ▐░▌ ▄  ▄▄▄▄█░█▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌
+▐░▌       ▐░▌▐░▌      ▐░░▌▐░░░░░░░▌    ▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░▌       ▐░▌▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+ ▀         ▀  ▀        ▀▀  ▀▀▀▀▀▀▀      ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀         ▀  ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀ 
+EOF
+
+echo "Installting AWS Linux Extra"
+echo "---------------------------"
+sudo amazon-linux-extras install -y aws-nitro-enclaves-cli
+echo "Installing AWS Nitro Dev and OpenSSL Env"
+sudo yum install -y aws-nitro-enclaves-cli-devel jq openssl11-libs
+echo -ne '>>>                       [20%]\r'
+sleep 2
+clear
+
+echo "Modifying EC2 User"
+echo "---------------------------"
+sudo usermod -aG ne ec2-user
+echo "Adding EC2-USER to Docker Group"
+sudo usermod -aG docker ec2-user
+echo -ne '>>>>>>>                   [40%]\r'
+sleep 2
+clear
+
+echo "Installing Docker"
+echo "---------------------------"
+sudo yum install -y docker
+echo -ne '>>>>>>>                   [45%]\r'
+sleep 2
+clear
+
+echo "Enable Docker"
+echo "---------------------------"
+sudo systemctl enable docker
+sleep 2
+echo -ne '>>>>>>>>>                 [50%]\r'
+clear 
+
+echo "Enable Kernel Module"
+echo "---------------------------"
+echo 'KERNEL=="vsock", MODE="660", GROUP="ne"' | sudo tee /etc/udev/rules.d/51-vsock.rules
+sleep 2
+echo -ne '>>>>>>>>>>>>>>            [60%]\r'
+clear 
+
+echo "Reloading UDEVADM" 
+echo "---------------------------"
+sudo udevadm control --reload
+echo -ne '>>>>>>>>>>>>>>            [65%]\r'
+
+clear 
+
+echo "Trigger UDEVADM"
+echo "---------------------------"
+sudo udevadm trigger
+sleep 2
+echo -ne '>>>>>>>>>>>>>>            [70%]\r'
+clear
+
+echo "Change Allocator Memory"
+echo "---------------------------"
+sudo sed -i 's/^memory_mib:.*/memory_mib: $MEMORY/' /etc/nitro_enclaves/allocator.yaml
+sleep 2
+echo -ne '>>>>>>>>>>>>>>            [75%]\r'
+clear
+
+echo "Starting Allocator"
+echo "---------------------------"
+sudo systemctl start nitro-enclaves-allocator.service
+sleep 2
+echo -ne '>>>>>>>>>>>>>>            [76%]\r'
+clear
+
+echo "Enable Allocator" 
+echo "---------------------------"
+sudo systemctl enable nitro-enclaves-allocator.service
+sleep 2
+echo -ne '>>>>>>>>>>>>>>            [78%]\r'
+clear
+
+echo "Start and Enable Docker"
+echo "---------------------------"
+sudo systemctl start docker && sudo systemctl enable docker
+sleep 2
+echo -ne '>>>>>>>>>>>>>>            [80%]\r'
+clear
+
+echo "Create Opt Directory"
+echo "---------------------------"
+sudo mkdir -p /opt/anjuna/nitro
+sleep 2
+echo -ne '>>>>>>>>>>>>>>>>          [84%]\r'
+clear
+
+echo "Download Binary"
+echo "---------------------------"
+wget https://api.downloads.anjuna.io/v1/releases/anjuna-nitro-runtime.$VERSION.tar.gz \
+  --header="X-Anjuna-Auth-Token:$APIKEY"
+sleep 2
+echo -ne '>>>>>>>>>>>>>>>>>>        [86%]\r'
+clear
+
+echo "Extract Binary"
+echo "---------------------------"
+sudo tar -xvoz -C /opt/anjuna/nitro -f anjuna-nitro-runtime.$VERSION.tar.gz
+sleep 2
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>   [90%]\r'
+clear
+
+echo "Net Cap Enabled"
+echo "---------------------------"
+sudo setcap cap_net_bind_service=+ep  /opt/anjuna/nitro/bin/anjuna-nitro-netd-parent
+sleep 2
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>  [94%]\r'
+clear
+
+echo "Export Variables"
+echo "---------------------------"
+export PATH=$PATH:/opt/anjuna/nitro/bin
+sleep 2
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>> [96%]\r'
+clear
+
+echo "Create Log Directory"
+echo "---------------------------"
+sudo mkdir -p /var/log/nitro_enclave
+sleep 2
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>> [98%]\r'
+clear
+
+echo "Completed"
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\r'
+echo -ne '\n'
